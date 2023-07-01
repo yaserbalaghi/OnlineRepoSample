872cebaed6d945d8a879ad540a87e214c43ed3b9
Yaser <eng.balaghi@yahoo.com>
2023-07-01 09:34:16 +0330

    Add project files.

---
+++ OnlineRepoSample/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.ForegroundColor = ConsoleColor.White;
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Clear();
+
+Console.WriteLine("Hi, Friend!");
+Console.WriteLine("I am a code from an online repository (Github).");
+
+Console.ReadKey();
